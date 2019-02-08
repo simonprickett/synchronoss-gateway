bbc86de0a63049f71dbe02917b2cad5ec8d86fcb
Simon Prickett <simon@crudworks.org>
2019-02-07 22:17:48 -0800

    Initial basic working version.

---
+++ README.md
@@ -0,0 +1,11 @@
+# Synchronoss Gateway for Conectric Sensors
+
+## Setup
+
+Plug a Conectric USB router into an available USB port.
+
+```
+$ npm install
+$ export APP_KEY=<key for API>
+$ npm start
+```
---
+++ server.js
@@ -0,0 +1,67 @@
+const gateway = require('conectric-usb-gateway')
+const moment = require('moment')
+const request = require('request')
+
+const APP_KEY = process.env.APP_KEY
+
+const stringifyObject = obj => {
+  const stringifiedObj = {}
+
+  for (const k in obj) {
+    stringifiedObj[k] = `${obj[k]}`
+  }
+
+  return stringifiedObj
+}
+
+const onSensorMessage = sensorMessage => {
+  try {
+    const messageDate = moment.unix(sensorMessage.timestamp).format('YYYY-MM-DD')
+    const eventId = `${sensorMessage.sensorId}${sensorMessage.sequenceNumber}`
+    const stringifiedPayload = stringifyObject(sensorMessage.payload)
+
+    const payload = {
+      event_type: sensorMessage.type,
+      timestamp: `${sensorMessage.timestamp}`,
+      sensorId: sensorMessage.sensorId,
+      sequenceNumber: `${sensorMessage.sequenceNumber}`,
+      ...stringifiedPayload
+    }
+
+    const uri = `https://siprt.synchronoss.net/events?APP_KEY=${APP_KEY}&APP_VERSION=1&APP_MODULE=sip-iotsb&EVENT_ID=${eventId}&EVENT_DATE=${messageDate}&EVENT_TYPE=${sensorMessage.type}`
+
+    console.log(uri)
+    console.log(payload)
+
+    request({
+      method: 'POST',
+      uri,
+      json: true,
+      body: payload
+    }, (err, res, body) => {
+      if (err) {
+        console.error('Error posting to API:')
+        console.error(err)
+      } else {
+        if (res.statusCode === 200 && body.result && body.result === 'success') {
+          console.log(`Sent message, registeredID: ${body.registeredID}`)
+        } else {
+          console.error(`Error posting to API, statusCode: ${res.statusCode}`)
+          console.error(res.body)
+        }
+      }
+    })
+  } catch (err) {
+    console.error('Error occurred sending message to API:')
+    console.error(err)
+  } 
+}
+
+if (! APP_KEY) {
+  console.error('You must set the APP_KEY environment variable!')
+  process.exit(1)
+}
+
+gateway.runGateway({
+  onSensorMessage
+})
